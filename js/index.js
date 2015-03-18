c5fad3823d7e1c36c0a97d6ec798bc4d80a694ba
AMMing <y2443@163.com>
2015-03-18 13:47:03 +0800

    juqery ext imgeffect

--- js/index.js
+++ js/index.js
@@ -2,83 +2,104 @@
 // create 2015.03.06 by aming
 //
 /////////////////////////////
-var AMingHomePage = function() {
-	var $objs, page, animate = null;
+var AMingHomePage = function () {
+    var $objs, page, animate = null;
 
-	//jQuery objects
-	$objs = {
-		window: null,
-		fix_content: null,
-		fix_left: null,
-		fix_right: null,
-		init: function() {
-			this.window = jQuery(window);
-			this.fix_content = jQuery('.fix_content');
-			this.fix_left = this.fix_content.find('.left');
-			this.fix_right = this.fix_content.find('.right');
+    //jQuery objects
+    $objs = {
+        window: null,
+        fix_content: null,
+        fix_left: null,
+        fix_right: null,
+        init: function () {
+            this.window = jQuery(window);
+            this.fix_content = jQuery('.fix_content');
+            this.fix_left = this.fix_content.find('.left');
+            this.fix_right = this.fix_content.find('.right');
 
-		}
-	};
-	this.$Objs = $objs;
+        }
+    };
+    this.$Objs = $objs;
 
-	//page js
-	page = {
-		resize: function() {
-			$objs.fix_left.width($objs.window.width() - 500);
+    //page js
+    page = {
+        resize: function () {
+            $objs.fix_left.width($objs.window.width() - 500);
 
-			animate.resetline();
-		},
-		init: function() {
-			$objs.window.bind('resize', this.resize);
-		}
-	};
-	this.Page = page;
+            animate.resetline();
+        },
+        init: function () {
+            $objs.window.bind('resize', this.resize);
+            page.resize();
+        }
+    };
+    this.Page = page;
 
-	animate = {
-		$line: null,
-		initline: function() {
-			this.line = $objs.fix_right.find('.bg').aming_line({
-				space: 20,
-				border: 3,
-				count: 5,
+    animate = {
+        $line: null,
+        $effect: null,
+        time: 3000,
+        initline: function () {
+            this.$line = $objs.fix_right.find('.bg').aming_line({
+                space: 20,
+                border: 2,
+                count: 6,
                 mintime: 400,
                 maxtime: 1200,
-				color: ['#45C0FF', '#FFAE5F', '#62EFA9', '#C45FFF', '#FF7197']
-			});
-		},
-		resetline: function() {
-			var index = this.line.showindex;
-			this.initline();
-			this.line.setIndex(index);
-		},
-		init: function() {
-			this.initline();
-			$('#btn_1').click(function() {
-				animate.line.pre();
-			});
-			$('#btn_2').click(function() {
-				animate.line.next();
-			});
-		}
-	};
-	this.Animate = animate;
+                color: ['#45C0FF', '#FFAE5F', '#62EFA9', '#C45FFF', '#FFEF80', '#FF7197'],
+                bgcolor: ['#D7F1FF', '#FFEFDF', '#D5FFEA', '#F4E0FF', '#FFFDF2', '#FFEAF0']
+            });
+            this.$effect = $objs.fix_right.find('.content').aming_imgeffect({
+                center: true,
+                margin: 60,
+                time: 800,
+                imgs: [
+                    'images/nav_1.png',
+                    'images/nav_2.png',
+                    'images/nav_3.png',
+                    'images/nav_4.png',
+                    'images/nav_5.png',
+                    'images/nav_6.png'
+                ]
+            });
+        },
+        resetline: function () {
+            if (this.$line == null)
+                return;
 
-	//init all object init functions.
-	this.Init = function() {
-		var props = AmingEx.getPropertys(this);
-		for (var i = 0; i < props.length; i++) {
-			var item = props[i];
-			if (item.type == 'object') {
-				AmingEx.execFunc(item.value, 'init');
-			}
-		};
-	};
+            var index = this.$line.showindex;
+            this.initline();
+            this.$line.setIndex(index);
+        },
+        auto: function () {
+            setInterval(function () {
+                animate.$line.next();
+                animate.$effect.next();
+            }, animate.time);
+        },
+        init: function () {
+            this.initline();
+            this.auto();
+        }
+    };
+    this.Animate = animate;
+
+    //init all object init functions.
+    this.Init = function () {
+        var props = AmingEx.getPropertys(this);
+        for (var i = 0; i < props.length; i++) {
+            var item = props[i];
+            if (item.type == 'object') {
+                AmingEx.execFunc(item.value, 'init');
+            }
+        };
+    };
 }
 
 
 
 var homepage = new AMingHomePage();
-$(function() {
-	homepage.Init();
+$(function () {
+    homepage.Init();
 
 });
--- js/line.js
+++ js/line.js
@@ -1,8 +1,12 @@
-(function(window, document, $, undefined) {
+//////////////////////////////
+// create 2015.03.17 by aming
+//
+/////////////////////////////
+(function (window, document, $, undefined) {
     $.extend($.fn, {
         color_list: ['#bf1729', '#e989e7', '#1a068f', '#518b8a', '#0d07e4', '#246092', '#a55e5b', '#ed1db8', '#7ca194', '#be41ca', '#4bfd77', '#2a7183', '#77d3d5', '#1829d6', '#481224', '#dce95a', '#8a64fd', '#917511', '#a4a682', '#dd03d3', '#dbcbfb', '#47f7fb', '#94682e', '#8dab23', '#fd9c84', '#53027f', '#d88025', '#17afb0', '#1680cf', '#d75d20', '#c812d1', '#2d2cb9', '#d028aa', '#ef7deb', '#f3da0f', '#9c1de6', '#43ee82', '#4f42e2', '#3e7c0c', '#e19279', '#60a61d', '#f47c89', '#bd66ed', '#0c542f', '#b0d363', '#b0fde6', '#32a8ee', '#e4c0c2', '#99cdb6', '#26cd15', '#512b4e', '#c47453', '#990294', '#f8bd5a', '#3c491a', '#5b9334', '#97e0a3', '#5b7441', '#b4563a', '#8d047d', '#7ec2bf', '#5f2290', '#1b8687', '#bc4b3e', '#9f16ad', '#2efa18', '#9bb8e7', '#a6c328', '#b22048', '#c01d2c', '#cb1efe', '#f6a4bf', '#9e9fc0', '#a39730', '#6d9ecd', '#386707', '#9f0f33', '#13c996', '#8e72d1', '#b52a15', '#f73445', '#e02ee6', '#e3c5f7', '#b92122', '#1f3d82', '#1c3784', '#d56fe5', '#b55e8a', '#096ad0', '#5d100f', '#06416a', '#b00597', '#8e9ed2', '#04ab0a', '#de4f30', '#0468d0', '#0288d9', '#70175d', '#eea859', '#922e03'],
 
-        aming_line: function(setting, callback) {
+        aming_line: function (setting, callback) {
             this.version = '1.0.0.0';
 
             var config = $.extend({
@@ -11,7 +15,8 @@
                 count: 3,
                 mintime: 200,
                 maxtime: 1000,
-                color: null
+                color: null,
+                bgcolor: null
             }, setting);
 
             var random_id = parseInt(99999 * Math.random());
@@ -21,9 +26,14 @@
 
             $renderTo.empty();
             $renderTo.amingLine = this;
-            $renderTo.data('rid', random_id);
+            $renderTo.attr('data-rid', random_id);
+
+            if ($renderTo.css('position') == 'static') {
+                $renderTo.css({
+                    position: 'relative'
+                });
+            }
             $renderTo.css({
-                position: 'relative',
                 overflow: 'hidden'
             });
 
@@ -34,45 +44,63 @@
             divList = new Array();
 
             console.log(column);
-            var randomColor = function() {
+            var randomColor = function () {
                 var index = Math.floor(Math.random() * $.fn.color_list.length);
 
                 return $.fn.color_list[index];
             };
-            var randomTime = function() {
+            var randomTime = function () {
                 return Math.floor(Math.random() * (config.maxtime - config.mintime) + config.mintime);
             };
 
-            var getColor = function(index) {
+            var getColor = function (index) {
                 if (config.color != null && config.color.length >= config.count) {
                     return config.color[index];
                 } else {
                     return randomColor();
                 }
             };
+            var getbgColor = function (index) {
+                if (config.bgcolor == null)
+                    return null;
 
-            var col_div = function(cindex, index, color) {
+                if (config.bgcolor.length >= config.count) {
+                    return config.bgcolor[index];
+                } else {
+                    return randomColor();
+                }
+            };
+
+            var col_div = function (cindex, index, color, bgColor) {
                 var $div = jQuery('<div></div>');
                 $div.width(config.space - config.border);
                 $div.height(h);
                 $div.addClass('col_div');
-                $div.attr('index', index);
-                $div.attr('cindex', cindex);
+                $div.attr('data-index', index);
+                $div.attr('data-cindex', cindex);
                 $div.css({
                     position: 'absolute',
                     top: cindex * h,
                     left: index * config.space,
-                    borderRight: config.border + 'px solid ' + color
+                    borderRight: config.border + 'px solid ' + color,
+                    background: bgColor
                 });
 
                 return $div;
             };
 
-            var col_divs = function(index, count) {
+            var col_divs = function (index, count) {
                 var color = getColor(index);
+                var bgColor = getbgColor(index);
                 for (var i = 0; i < column; i++) {
-                    $renderTo.append(col_div(index, i, color));
+                    $renderTo.append(col_div(index, i, color, bgColor));
                 }
+            };
+            var getColumnDivByIndex = function (index) {
+                return $renderTo.find('.col_div[data-index="' + index + '"]');
+            };
+            var getColumnDivByCindex = function ($col_items, cindex) {
+                return $col_items.filter('.col_div[data-cindex="' + cindex + '"]');
             };
 
             for (var i = 0; i < config.count; i++) {
@@ -81,7 +109,7 @@
 
             obj.showindex = 0;
 
-            var animate_top = function() {
+            var animate_top = function () {
                 var oldindex = obj.showindex;
                 var newindex = obj.showindex + 1;
                 obj.showindex = newindex;
@@ -91,9 +119,9 @@
                 obj.showindex = newindex;
 
                 for (var i = 0; i < column; i++) {
-                    var $col_items = $renderTo.find('.col_div[index="' + i + '"]');
-                    var $items_old = $col_items.filter('.col_div[cindex="' + oldindex + '"]');
-                    var $items_new = $col_items.filter('.col_div[cindex="' + newindex + '"]');
+                    var $col_items = getColumnDivByIndex(i);
+                    var $items_old = getColumnDivByCindex($col_items, oldindex);
+                    var $items_new = getColumnDivByCindex($col_items, newindex);
                     var time = randomTime();
                     $col_items.css({
                         top: h
@@ -112,7 +140,9 @@
                 }
 
             }
-            var animate_bottom = function() {
+
+
+            var animate_bottom = function () {
                 var oldindex = obj.showindex;
                 var newindex = obj.showindex - 1;
                 if (newindex < 0) {
@@ -121,9 +151,9 @@
                 obj.showindex = newindex;
 
                 for (var i = 0; i < column; i++) {
-                    var $col_items = $renderTo.find('.col_div[index="' + i + '"]');
-                    var $items_old = $col_items.filter('.col_div[cindex="' + oldindex + '"]');
-                    var $items_new = $col_items.filter('.col_div[cindex="' + newindex + '"]');
+                    var $col_items = getColumnDivByIndex(i);
+                    var $items_old = getColumnDivByCindex($col_items, oldindex);
+                    var $items_new = getColumnDivByCindex($col_items, newindex);
                     var time = randomTime();
                     $col_items.css({
                         top: -1 * h
@@ -142,7 +172,7 @@
                 }
 
             }
-            this.setIndex = function(index) {
+            this.setIndex = function (index) {
                 if (index == obj.showindex)
                     return;
 
@@ -163,10 +193,10 @@
                     animate_top();
                 }
             };
-            this.pre = function() {
+            this.pre = function () {
                 animate_top();
             };
-            this.next = function() {
+            this.next = function () {
                 animate_bottom();
             };
 
